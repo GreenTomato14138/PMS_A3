60f6d5671623d6b49b12d7012a7e064d0a418e06
kid <3480800065@qq.com>
2026-04-26 21:17:19 +0800

    Add files via upload

---
+++ app.routes.ts
@@ -0,0 +1,9 @@
+// app/app.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then(m => m.routes)
+  }
+];
